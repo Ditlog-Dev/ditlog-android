0500518a73eae833a4f334e2af4ca49b6bef5c17
candrahesen <candrahesen@gmail.com>
2018-03-01 19:26:30 +0700

    add recycler in fragment for showing the indicator

--- app/src/main/java/id/ac/itb/ditlog/monitorandperformance/ChooseIndicator.java
+++ app/src/main/java/id/ac/itb/ditlog/monitorandperformance/ChooseIndicator.java
@@ -3,9 +3,13 @@ package id.ac.itb.ditlog.monitorandperformance;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 
 
 /**
@@ -14,8 +18,33 @@ import android.view.ViewGroup;
 public class ChooseIndicator extends Fragment {
 
 
-    public ChooseIndicator() {
-        // Required empty public constructor
+    private static final String TAG = "RecyclerViewFragment";
+    private static final String KEY_LAYOUT_MANAGER = "layoutManager";
+    private static final int SPAN_COUNT = 2;
+    private static final int DATASET_COUNT = 60; // menampilkan data sebanyak value
+
+    private enum LayoutManagerType {
+        GRID_LAYOUT_MANAGER,
+        LINEAR_LAYOUT_MANAGER
+    }
+
+    protected LayoutManagerType mCurrentLayoutManagerType;
+
+    protected RecyclerView recyclerViewIndicator;
+    protected RecyclerChooseIndicator indicatorAdapter;
+    protected RecyclerView.LayoutManager indicatorLayoutManager;
+    protected String[] mParam;
+
+    String[] param_indicator = {"Kedisiplinan", "Ketepatan Waktu", "Kelengkapan", "Terdokumentasi"};
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Initialize dataset, this data would usually come from a local content provider or
+        // remote server.
+        initDataset();
     }
 
 
@@ -23,7 +52,98 @@ public class ChooseIndicator extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_choose_indicator, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_choose_indicator, container, false);
+
+        rootView.setTag(TAG);
+
+        // BEGIN_INCLUDE(initializeRecyclerView)
+        recyclerViewIndicator = (RecyclerView) rootView.findViewById(R.id.recyclerViewIndicator);
+
+        // LinearLayoutManager is used here, this will layout the elements in a similar fashion
+        // to the way ListView would layout elements. The RecyclerView.LayoutManager defines how
+        // elements are laid out.
+        indicatorLayoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
+
+        mCurrentLayoutManagerType = LayoutManagerType.LINEAR_LAYOUT_MANAGER;
+
+        if (savedInstanceState != null) {
+            // Restore saved layout manager type.
+            mCurrentLayoutManagerType = (LayoutManagerType) savedInstanceState
+                    .getSerializable(KEY_LAYOUT_MANAGER);
+        }
+        setRecyclerViewLayoutManager(mCurrentLayoutManagerType);
+
+        indicatorAdapter = new RecyclerChooseIndicator(mParam);
+
+        // Set CustomAdapter as the adapter for RecyclerView.
+        recyclerViewIndicator.setAdapter(indicatorAdapter);
+
+        // END_INCLUDE(initializeRecyclerView)
+
+        return rootView;
     }
 
+    public void setRecyclerViewLayoutManager(LayoutManagerType layoutManagerType) {
+        int scrollPositionParamIndicator = 0;
+
+        // If a layout manager has already been set, get current scroll position.
+        if (recyclerViewIndicator.getLayoutManager() != null) {
+            scrollPositionParamIndicator = ((LinearLayoutManager) recyclerViewIndicator.getLayoutManager())
+                    .findFirstCompletelyVisibleItemPosition();
+        }
+
+        switch (layoutManagerType) {
+            case GRID_LAYOUT_MANAGER:
+                indicatorLayoutManager = new GridLayoutManager(getActivity(), SPAN_COUNT);
+                mCurrentLayoutManagerType = LayoutManagerType.GRID_LAYOUT_MANAGER;
+                break;
+            case LINEAR_LAYOUT_MANAGER:
+                indicatorLayoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
+                mCurrentLayoutManagerType = LayoutManagerType.LINEAR_LAYOUT_MANAGER;
+                break;
+            default:
+                indicatorLayoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
+                mCurrentLayoutManagerType = LayoutManagerType.LINEAR_LAYOUT_MANAGER;
+        }
+
+        recyclerViewIndicator.setLayoutManager(indicatorLayoutManager);
+
+        recyclerViewIndicator.scrollToPosition(scrollPositionParamIndicator);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        // Save currently selected layout manager.
+        savedInstanceState.putSerializable(KEY_LAYOUT_MANAGER, mCurrentLayoutManagerType);
+        super.onSaveInstanceState(savedInstanceState);
+    }
+
+    /**
+     * Generates Strings for RecyclerView's adapter. This data would usually come
+     * from a local content provider or remote server.
+     */
+    private void initDataset() {
+        mParam = new String[param_indicator.length];
+        for (int i = 0; i < param_indicator.length; i++) {
+            mParam[i] = param_indicator[i];
+        }
+    }
+
+    public void onCheckboxClicked(View view) {
+        // Is the view now checked?
+        boolean checked = ((CheckBox) view).isChecked();
+
+        // Check which checkbox was clicked
+        switch (view.getId()) {
+            case R.id.checkbox_indicator:
+                if (checked) {
+
+                }
+                else {
+
+                }
+                break;
+
+        }
+    }
 }
