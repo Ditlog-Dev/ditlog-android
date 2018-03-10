e5db47dd6f84ccc12e4ce141f6039c537af5eb7b
Ida Ayu Putu Ari Crisdayanti <13515067@std.stei.itb.ac.id>
2018-03-10 10:59:22 +0700

    add connection and server availability handling

--- app/src/main/java/id/ac/itb/ditlog/monitorandperformance/AsyncAddIndicator.java
+++ app/src/main/java/id/ac/itb/ditlog/monitorandperformance/AsyncAddIndicator.java
@@ -1,13 +1,14 @@
 package id.ac.itb.ditlog.monitorandperformance;
 
+import android.app.Activity;
 import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.AsyncTask;
-import android.util.Log;
 import android.widget.Toast;
 
 import org.json.JSONObject;
 import java.io.DataOutputStream;
-import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
@@ -17,58 +18,61 @@ import java.net.URL;
 
 public class AsyncAddIndicator extends AsyncTask<String,Void,String> {
     public static final String SERVER_URL = "159.65.131.168:8080";
-    public static final int READ_TIMEOUT = 3000;
-    public static final int CONNECTION_TIMEOUT = 3000;
+    public static final int READ_TIMEOUT = 1500;
+    public static final int CONNECTION_TIMEOUT = 1500;
     private String indicator;
     private String status = "init";
     private Context mContext;
+    private Activity act;
 
-    public AsyncAddIndicator(String indicator, final Context context) {
+    public AsyncAddIndicator(String indicator, final Context context, Activity act) {
         mContext = context;
         this.indicator = indicator;
+        this.act = act;
     }
 
     @Override
     protected String doInBackground(String... params) {
         HttpURLConnection urlConnection = null;
-        try {
-            // build jsonObject
-            JSONObject ind = new JSONObject();
-            ind.put("name", indicator);
+        if (!haveNetworkConnection()) {
+            status = "Tidak ada koneksi internet";
+            return "Tidak ada koneksi internet";
+        } else {
+            try {
+                // build jsonObject
+                JSONObject ind = new JSONObject();
+                ind.put("name", indicator);
 
-            URL url = new URL("http://" + SERVER_URL + "/indicators");
-            urlConnection = (HttpURLConnection) url.openConnection();
-            // read response
-            urlConnection.setRequestMethod("POST");
-            urlConnection.setReadTimeout(READ_TIMEOUT);
-            urlConnection.setConnectTimeout(CONNECTION_TIMEOUT);
-            urlConnection.setDoOutput(true);
-            urlConnection.setDoInput(true);
-            urlConnection.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
-            //urlConnection.getOutputStream();
-            DataOutputStream wr = new DataOutputStream(urlConnection.getOutputStream());
-            wr.writeBytes(ind.toString());
-            wr.flush();
-            wr.close();
-            // try to get response
-            int statusCode = urlConnection.getResponseCode();
-            if (statusCode == 200) {
-                status = "Indikator berhasil ditambahkan";
-            } else {
-                status = "Operasi gagal";
+                URL url = new URL("http://" + SERVER_URL + "/indicators");
+                urlConnection = (HttpURLConnection) url.openConnection();
+                // read response
+                urlConnection.setRequestMethod("POST");
+                urlConnection.setReadTimeout(READ_TIMEOUT);
+                urlConnection.setConnectTimeout(CONNECTION_TIMEOUT);
+                urlConnection.setDoOutput(true);
+                urlConnection.setDoInput(true);
+                urlConnection.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+
+                DataOutputStream wr = new DataOutputStream(urlConnection.getOutputStream());
+                wr.writeBytes(ind.toString());
+                wr.flush();
+                wr.close();
+                // try to get response
+                int statusCode = urlConnection.getResponseCode();
+                if (statusCode == 200) {
+                    status = "Indikator berhasil ditambahkan";
+                } else {
+                    status = "Server tidak tersedia";
+                }
+            } catch (Exception e) {
+                status = "Server tidak tersedia";
+                e.printStackTrace();
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                return status;
             }
-        }
-        catch(Exception e)
-        {
-            status = "Operasi gagal";
-            e.printStackTrace();
-        }
-        finally
-        {
-            if (urlConnection != null) {
-                urlConnection.disconnect();
-            }
-            return status;
         }
     }
 
@@ -76,7 +80,24 @@ public class AsyncAddIndicator extends AsyncTask<String,Void,String> {
     protected void onPostExecute(String result) {
         super.onPostExecute(result);
         if (!status.equals("init")) {
-            Toast.makeText(mContext, status, Toast.LENGTH_SHORT).show();
+            Toast.makeText(mContext, status, Toast.LENGTH_LONG).show();
         }
+    }
+
+    private boolean haveNetworkConnection() {
+        boolean haveConnectedWifi = false;
+        boolean haveConnectedMobile = false;
+
+        ConnectivityManager cm = (ConnectivityManager) act.getSystemService(mContext.CONNECTIVITY_SERVICE);
+        NetworkInfo[] netInfo = cm.getAllNetworkInfo();
+        for (NetworkInfo ni : netInfo) {
+            if (ni.getTypeName().equalsIgnoreCase("WIFI"))
+                if (ni.isConnected())
+                    haveConnectedWifi = true;
+            if (ni.getTypeName().equalsIgnoreCase("MOBILE"))
+                if (ni.isConnected())
+                    haveConnectedMobile = true;
+        }
+        return haveConnectedWifi || haveConnectedMobile;
     }
 }
--- app/src/main/java/id/ac/itb/ditlog/monitorandperformance/ChooseIndicator.java
+++ app/src/main/java/id/ac/itb/ditlog/monitorandperformance/ChooseIndicator.java
@@ -2,6 +2,8 @@ package id.ac.itb.ditlog.monitorandperformance;
 
 
 import android.content.DialogInterface;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -11,33 +13,31 @@ import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.JsonReader;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
 import android.widget.EditText;
-
+import android.widget.Toast;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.List;
-
+import java.util.concurrent.ExecutionException;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ChooseIndicator extends Fragment {
 
-
     private static final String TAG = "RecyclerViewFragment";
     private static final String KEY_LAYOUT_MANAGER = "layoutManager";
     private static final int SPAN_COUNT = 2;
-    private static final int DATASET_COUNT = 60; // menampilkan data sebanyak value
     private FloatingActionButton fab;
+    private HttpURLConnection connection = null;
 
     private enum LayoutManagerType {
         GRID_LAYOUT_MANAGER,
@@ -49,18 +49,11 @@ public class ChooseIndicator extends Fragment {
     protected RecyclerView recyclerViewIndicator;
     protected RecyclerChooseIndicator indicatorAdapter;
     protected RecyclerView.LayoutManager indicatorLayoutManager;
-    protected String[] mParam;
-
-    String[] param_indicator = {"Kedisiplinan", "Ketepatan Waktu", "Kelengkapan", "Terdokumentasi"};
-
+    protected ArrayList<IndicatorEntity> mParam = new ArrayList<>();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Initialize dataset, this data would usually come from a local content provider or
-        // remote server.
-        //initDataset();
     }
 
     protected void showInputDialog() {
@@ -77,7 +70,17 @@ public class ChooseIndicator extends Fragment {
                 .setPositiveButton("TAMBAH", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
                         String indicator = editText.getText().toString();
-                        new AsyncAddIndicator(indicator, getContext()).execute(indicator);
+                        String status = "fail";
+                        try {
+                            status = new AsyncAddIndicator(indicator, getContext(), getActivity()).execute(indicator).get();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        } catch (ExecutionException e) {
+                            e.printStackTrace();
+                        }
+                        if (status.equals("Indikator berhasil ditambahkan")) {
+                            new indicatorGetter().execute();
+                        }
                     }
                 })
                 .setNegativeButton("BATAL",
@@ -99,6 +102,7 @@ public class ChooseIndicator extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_choose_indicator, container, false);
 
         rootView.setTag(TAG);
+
         //fab tambah indikator baru
         fab = (FloatingActionButton) rootView.findViewById(R.id.tambah);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -126,10 +130,13 @@ public class ChooseIndicator extends Fragment {
         }
         setRecyclerViewLayoutManager(mCurrentLayoutManagerType);
 
-
         new indicatorGetter().execute();
 
         // END_INCLUDE(initializeRecyclerView)
+
+        if (!haveNetworkConnection()) {
+            Toast.makeText(getContext(), "Tidak ada koneksi internet", Toast.LENGTH_LONG).show();
+        }
 
         return rootView;
     }
@@ -169,19 +176,6 @@ public class ChooseIndicator extends Fragment {
         super.onSaveInstanceState(savedInstanceState);
     }
 
-    /**
-     * Generates Strings for RecyclerView's adapter. This data would usually come
-     * from a local content provider or remote server.
-     */
-    private void initDataset() {
-
-        mParam = new String[param_indicator.length];
-        for (int i = 0; i < param_indicator.length; i++) {
-            mParam[i] = param_indicator[i];
-        }
-
-    }
-
     public void onCheckboxClicked(View view) {
         // Is the view now checked?
         boolean checked = ((CheckBox) view).isChecked();
@@ -200,19 +194,34 @@ public class ChooseIndicator extends Fragment {
         }
     }
 
-    public class indicatorGetter extends AsyncTask<Void, Void, String[]>{
+    public class indicatorGetter extends AsyncTask<Void, Void, ArrayList<IndicatorEntity>>{
 
         public static final String SERVER_URL = "159.65.131.168:8080";
         public static final int READ_TIMEOUT = 15000;
         public static final int CONNECTION_TIMEOUT = 15000;
 
+        public int pageNumber=0;
+        public int itemLimit=20;
+        public String direction="asc";
+        public String sortingKey="id";
+
+        ArrayList<IndicatorEntity> params = new ArrayList<>();
+        int totalPages=-1;
+        int totalElements=-1;
+        int numberOfElements=-1;
+        int number=-1;
+
         @Override
-        protected String[] doInBackground(Void... voids) {
-            List<String> params = new ArrayList<>();
+        protected ArrayList<IndicatorEntity> doInBackground(Void... voids) {
             String method = "GET";
             try {
-                URL url = new URL("http://" + SERVER_URL + "/indicators");
-                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                String rawUrl = "http://" + SERVER_URL + "/indicators";
+                rawUrl += "?page=" + String.valueOf(pageNumber);
+                rawUrl += "&limit=" + String.valueOf(itemLimit);
+                rawUrl +="&dir=" + direction;
+                rawUrl +="&sort=" + sortingKey;
+                URL url = new URL(rawUrl);
+                connection = (HttpURLConnection) url.openConnection();
 
                 connection.setRequestMethod(method);
                 connection.setReadTimeout(READ_TIMEOUT);
@@ -227,77 +236,100 @@ public class ChooseIndicator extends Fragment {
                     JsonReader jsReader = new JsonReader(isReader);
 
                     jsReader.beginObject();
-                    jsReader.nextName();
-                    boolean status = jsReader.nextBoolean();
-                    jsReader.nextName();
-                    int code = jsReader.nextInt();
-                    jsReader.nextName();
-                    try{
-                        jsReader.nextString();
-                    }
-                    catch (Exception e){
-                        jsReader.nextNull();
-                    }
-                    jsReader.nextName();
-                    jsReader.beginObject();
-                    jsReader.nextName();
-                    jsReader.beginArray();
                     while(jsReader.hasNext()){
-                        jsReader.beginObject();
-                        jsReader.nextName();
-                        int id = jsReader.nextInt();
-                        jsReader.nextName();
-                        String name = jsReader.nextString();
-                        params.add(name);
-                        jsReader.nextName();
-                        try{
-                            jsReader.nextInt();
+                        String name = jsReader.nextName();
+                        if(name.equals("payload")){
+                            parsePayload(jsReader);
                         }
-                        catch (Exception e){
-                            jsReader.nextNull();
+                        else{
+                            jsReader.skipValue();
                         }
-                        jsReader.endObject();
                     }
-                    jsReader.endArray();
-                    jsReader.nextName();
-                    boolean last = jsReader.nextBoolean();
-                    jsReader.nextName();
-                    int totalElements = jsReader.nextInt();
-                    jsReader.nextName();
-                    int totalPages = jsReader.nextInt();
-                    jsReader.nextName();
-                    int totalsize = jsReader.nextInt();
-                    jsReader.nextName();
-                    int totalnumber = jsReader.nextInt();
-                    jsReader.nextName();
-                    String sort = null;
-                    try {
-                        sort = jsReader.nextString();
-                    }
-                    catch(Exception e){
-                        jsReader.nextNull();
-                    }
-                    jsReader.nextName();
-                    boolean first = jsReader.nextBoolean();
-                    jsReader.nextName();
-                    int numberOfElements = jsReader.nextInt();
-                    jsReader.endObject();
                     jsReader.endObject();
                 }
-
-
-            } catch (MalformedURLException e) {
+            } catch (Exception e) {
                 e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
+                Toast.makeText(getContext(), "Server tidak tersedia", Toast.LENGTH_LONG).show();
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+                return params;
             }
+        }
+
+        protected void parsePayload(JsonReader jsReader) throws IOException{
+
+            String name;
+            jsReader.beginObject();
+            while(jsReader.hasNext()) {
+                name = jsReader.nextName();
+                if(name.equals("content")) {
+                    parseContent(jsReader);
+                }
+                else if(name.equals("totalPages")){
+                    totalPages = jsReader.nextInt();
+                }
+                else if(name.equals("totalElements")){
+                    totalElements = jsReader.nextInt();
+                }
+                else if(name.equals("numberOfElements")){
+                    numberOfElements = jsReader.nextInt();
+                }
+                else if(name.equals("number")){
+                    number = jsReader.nextInt();
+                }
+                else{
+                    jsReader.skipValue();
+                }
+            }
+            jsReader.endObject();
+
+            Log.d("totalPages", String.valueOf(totalPages));
+            Log.d("totalElements", String.valueOf(totalElements));
+            Log.d("numberOfElements", String.valueOf(numberOfElements));
+            Log.d("number", String.valueOf(number));
+        }
 
 
-            return params.toArray(new String[0]);
+        protected void parseContent(JsonReader jsReader)throws IOException{
+            String name;
+            jsReader.beginArray();
+            while (jsReader.hasNext()) {
+                jsReader.beginObject();
+                int id=-1;
+                String indName="";
+                int idUser=-1;
+
+                while (jsReader.hasNext()) {
+                    name = jsReader.nextName();
+                    switch (name){
+                        case "id":
+                            id = jsReader.nextInt();
+                            break;
+                        case "name":
+                            indName = jsReader.nextString();
+                            break;
+                        case "idUser":
+                            try {
+                                idUser = jsReader.nextInt();
+                            } catch (RuntimeException e) {
+                                jsReader.skipValue();
+                            }
+                            break;
+                        default:
+                            jsReader.skipValue();
+                    }
+                }
+                params.add(new IndicatorEntity(id,indName,idUser));
+                jsReader.endObject();
+            }
+            jsReader.endArray();
+
         }
 
         @Override
-        protected void onPostExecute(String[] mParam) {
+        protected void onPostExecute(ArrayList<IndicatorEntity> mParam) {
             super.onPostExecute(mParam);
 
             indicatorAdapter = new RecyclerChooseIndicator(mParam);
@@ -305,5 +337,22 @@ public class ChooseIndicator extends Fragment {
             // Set CustomAdapter as the adapter for RecyclerView.
             recyclerViewIndicator.setAdapter(indicatorAdapter);
         }
+    }
+
+    private boolean haveNetworkConnection() {
+        boolean haveConnectedWifi = false;
+        boolean haveConnectedMobile = false;
+
+        ConnectivityManager cm = (ConnectivityManager) getActivity().getSystemService(getContext().CONNECTIVITY_SERVICE);
+        NetworkInfo[] netInfo = cm.getAllNetworkInfo();
+        for (NetworkInfo ni : netInfo) {
+            if (ni.getTypeName().equalsIgnoreCase("WIFI"))
+                if (ni.isConnected())
+                    haveConnectedWifi = true;
+            if (ni.getTypeName().equalsIgnoreCase("MOBILE"))
+                if (ni.isConnected())
+                    haveConnectedMobile = true;
+        }
+        return haveConnectedWifi || haveConnectedMobile;
     }
 }
