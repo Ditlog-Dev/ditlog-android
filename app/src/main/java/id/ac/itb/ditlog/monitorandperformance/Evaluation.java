ce9ad262bbeda160e0a89fb2366834178222aee5
candrahesen <candrahesen@gmail.com>
2018-04-07 09:21:56 +0700

    add fragment evaluation

--- app/src/main/java/id/ac/itb/ditlog/monitorandperformance/Evaluation.java
+++ app/src/main/java/id/ac/itb/ditlog/monitorandperformance/Evaluation.java
@@ -3,6 +3,9 @@ package id.ac.itb.ditlog.monitorandperformance;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,16 +17,123 @@ import android.view.ViewGroup;
 public class Evaluation extends Fragment {
 
 
-    public Evaluation() {
-        // Required empty public constructor
+    private static final String TAG = "RecyclerViewFragment";
+    private static final String KEY_LAYOUT_MANAGER = "layoutManager";
+    private static final int SPAN_COUNT = 2;
+    private static final int DATASET_COUNT = 60; // menampilkan data sebanyak value
+
+    private enum LayoutManagerType {
+        GRID_LAYOUT_MANAGER,
+        LINEAR_LAYOUT_MANAGER
     }
+
+    protected LayoutManagerType mCurrentLayoutManagerType;
+
+    protected RecyclerView evaluationRecyclerView;
+    protected RecyclerEvaluation evaluationAdapter;
+    protected RecyclerView.LayoutManager evaluationLayoutManager;
+    protected String[] mDataset, mDataset2;
+
+    String [] indicatorEvaluation = {"Kampus Mahasiswa Indicator Test","Kampus Mahasiswa Indicator Test","Kampus Mahasiswa Indicator Test"};
+    String [] grade = {"80","60","90"};
 
 
     @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Initialize dataset, this data would usually come from a local content provider or
+        // remote server.
+        initDataset();
+    }
+    /*
+        public DaftarProdukFragment() {
+            // Required empty public constructor
+        }
+    */
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_evaluation, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_evaluation, container, false);
+        rootView.setTag(TAG);
+
+        // BEGIN_INCLUDE(initializeRecyclerView)
+        evaluationRecyclerView = (RecyclerView) rootView.findViewById(R.id.recyclerViewEvaluation);
+
+        // LinearLayoutManager is used here, this will layout the elements in a similar fashion
+        // to the way ListView would layout elements. The RecyclerView.LayoutManager defines how
+        // elements are laid out.
+        evaluationLayoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
+
+        mCurrentLayoutManagerType = LayoutManagerType.LINEAR_LAYOUT_MANAGER;
+
+        if (savedInstanceState != null) {
+            // Restore saved layout manager type.
+            mCurrentLayoutManagerType = (LayoutManagerType) savedInstanceState
+                    .getSerializable(KEY_LAYOUT_MANAGER);
+        }
+        setRecyclerViewLayoutManager(mCurrentLayoutManagerType);
+
+        evaluationAdapter = new RecyclerEvaluation(mDataset,mDataset2);
+
+        // Set CustomAdapter as the adapter for RecyclerView.
+        evaluationRecyclerView.setAdapter(evaluationAdapter);
+
+        // END_INCLUDE(initializeRecyclerView)
+
+        return rootView;
+    }
+
+    /**
+     * Set RecyclerView's LayoutManager to the one given.
+     *
+     * @param layoutManagerType Type of layout manager to switch to.
+     */
+    public void setRecyclerViewLayoutManager(LayoutManagerType layoutManagerType) {
+        int scrollPositionEvaluation = 0;
+
+        // If a layout manager has already been set, get current scroll position.
+        if (evaluationRecyclerView.getLayoutManager() != null) {
+            scrollPositionEvaluation = ((LinearLayoutManager) evaluationRecyclerView.getLayoutManager())
+                    .findFirstCompletelyVisibleItemPosition();
+        }
+
+        switch (layoutManagerType) {
+            case GRID_LAYOUT_MANAGER:
+                evaluationLayoutManager = new GridLayoutManager(getActivity(), SPAN_COUNT);
+                mCurrentLayoutManagerType = LayoutManagerType.GRID_LAYOUT_MANAGER;
+                break;
+            case LINEAR_LAYOUT_MANAGER:
+                evaluationLayoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
+                mCurrentLayoutManagerType = LayoutManagerType.LINEAR_LAYOUT_MANAGER;
+                break;
+            default:
+                evaluationLayoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
+                mCurrentLayoutManagerType = LayoutManagerType.LINEAR_LAYOUT_MANAGER;
+        }
+
+        evaluationRecyclerView.setLayoutManager(evaluationLayoutManager);
+
+        evaluationRecyclerView.scrollToPosition(scrollPositionEvaluation);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        // Save currently selected layout manager.
+        savedInstanceState.putSerializable(KEY_LAYOUT_MANAGER, mCurrentLayoutManagerType);
+        super.onSaveInstanceState(savedInstanceState);
+    }
+    /**
+     * Generates Strings for RecyclerView's adapter. This data would usually come
+     * from a local content provider or remote server.
+     */
+    private void initDataset() {
+        mDataset = new String[indicatorEvaluation.length];
+        mDataset2 = new String[grade.length];
+        for (int i = 0; i < indicatorEvaluation.length; i++) {
+            mDataset[i] = indicatorEvaluation[i];
+            mDataset2[i] = grade[i];
+        }
     }
 
 }
