a42200c078f2ed6e474d8eb5933b1c5946c4142f
candrahesen <candrahesen@gmail.com>
2018-04-09 08:07:41 +0700

    add model class for evaluation

---
+++ app/src/main/java/id/ac/itb/ditlog/monitorandperformance/EvaluationEntity.java
@@ -0,0 +1,39 @@
+package id.ac.itb.ditlog.monitorandperformance;
+
+/**
+ * Created by ASUS on 09/04/2018.
+ */
+
+public class EvaluationEntity {
+    private String paramEvaluation;
+    private String gradeEvaluation;
+
+    public int id;
+
+    public EvaluationEntity(){
+        id=-1;
+        paramEvaluation="";
+        gradeEvaluation="100";
+    }
+    public EvaluationEntity(int id, String name, String grade){
+        this.id = id;
+        this.paramEvaluation = name;
+        this.gradeEvaluation = grade;
+    }
+
+    public String getParamEvaluation() {
+        return paramEvaluation;
+    }
+
+    public void setParamEvaluation(String paramEvaluation) {
+        this.paramEvaluation = paramEvaluation;
+    }
+
+    public String getGradeEvaluation() {
+        return gradeEvaluation;
+    }
+
+    public void setGradeEvaluation(String gradeEvaluation) {
+        this.gradeEvaluation = gradeEvaluation;
+    }
+}
